8495968fa274c1b68905b35c00c6824a0d00e4ed
bourne <xuhui.yon@gmail.com>
2023-10-08 11:39:35 +0800

    build

---
+++ baseSite/assets/error-legacy-d2d62048.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-6575fc12.js"],(function(e,r){"use strict";var t,n,u;return{setters:[function(e){t=e._,n=e.c,u=e.o}],execute:function(){e("default",t({},[["render",function(e,r){return u(),n("div",null,"error")}]]))}}}));
---
+++ baseSite/assets/test-fba43ecc.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./index-9a24205a.js";const n={};function o(r,s){return c(),t("div",null,"test")}const a=e(n,[["render",o]]);export{a as default};
