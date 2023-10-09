a797c2e369656009a4ed6aab318d2e3af6957d32
bourne <xuhui.yon@gmail.com>
2023-10-09 14:59:58 +0800

    del build

--- basefront/assets/local-list-77bea320.js
+++
@@ -1,1 +0,0 @@
-import{_ as c,c as e,o}from"./index-9a24205a.js";const t={};function l(n,r){return o(),e("div",null,"local-list")}const a=c(t,[["render",l]]);export{a as default};
--- basefront/assets/prerender-4b20f787.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as r,o as c}from"./index-9a24205a.js";const n={};function o(t,s){return c(),r("div",null,"基座预留prerender页面")}const a=e(n,[["render",o]]);export{a as default};
