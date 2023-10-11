64dc089983fec3863a318ae9535ddee0c3b69b0c
bourne <xuhui.yon@gmail.com>
2023-10-11 12:11:26 +0800

    del build

--- basefront/assets/local-detail-ffc50310.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o}from"./index-c320cef9.js";const t={};function a(l,n){return o(),c("div",null,"local-detail")}const s=e(t,[["render",a]]);export{s as default};
--- basefront/assets/multiple-b4adc714.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c,o as t}from"./index-c320cef9.js";const n={};function o(r,s){return t(),c("div",null,"多个应用")}const a=e(n,[["render",o]]);export{a as default};
