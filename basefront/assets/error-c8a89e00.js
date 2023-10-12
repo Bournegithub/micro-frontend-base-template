cac11ffbda4de58f6709803a7c436dc8a0eb544f
bourne <xuhui.yon@gmail.com>
2023-10-12 15:52:51 +0800

    build

---
+++ basefront/assets/error-c8a89e00.js
@@ -0,0 +1,1 @@
+import{_ as r,c as e,o as c}from"./index-332f98af.js";const o={};function n(t,s){return c(),e("div",null,"error")}const a=r(o,[["render",n]]);export{a as default};
---
+++ basefront/assets/local-detail-75229614.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o}from"./index-332f98af.js";const t={};function a(l,n){return o(),c("div",null,"local-detail")}const s=e(t,[["render",a]]);export{s as default};
