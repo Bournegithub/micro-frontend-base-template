6cdecf463fcf2cccc51eaf3a54ff5cbfe44a2202
bourne <xuhui.yon@gmail.com>
2023-10-27 11:03:24 +0800

    路由优化

--- src/views/local-list.vue
+++ src/views/local-list.vue
@@ -1,3 +1,13 @@
 <template>
 	<div>local-list</div>
 </template>
+
+<script setup lang="ts">
+	import { useHead } from '@unhead/vue';
+	useHead({
+  title: 'own list',
+		meta: [
+			{ name: 'description', content: 'own list page' },
+		],
+	})
+</script>
