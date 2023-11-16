60d7fc3ad6f0353476b0bb120de52a9f066e1fb3
bourne <xuhui.yon@gmail.com>
2023-11-16 16:37:46 +0800

    next14 build

---
+++ basefront/assets/local-list-da35ea52.js
@@ -0,0 +1,1 @@
+import{u as t}from"./vue.f36acd1f-894fcf62.js";import{d as s,c as a,o}from"./index-1769f8f1.js";const e=s({__name:"local-list",setup:s=>(t({title:"own list",meta:[{name:"description",content:"own list page"}]}),(t,s)=>(o(),a("div",null,"local-list")))});export{e as default};
---
+++ basefront/assets/prerender-1f4159fd.js
@@ -0,0 +1,1 @@
+import{u as e}from"./vue.f36acd1f-894fcf62.js";import{d as r,c as n,o as t}from"./index-1769f8f1.js";const a=r({__name:"prerender",setup:r=>(e({title:"prerender",meta:[{name:"description",content:"prerender page"}]}),(e,r)=>(t(),n("div",null,"基座预留prerender页面")))});export{a as default};
